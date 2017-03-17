d5c85908063cbbcba970d1b832b0c14c16ba2f05
alladmin <alladmin@ubuntuMain3.0>
2017-03-17 13:02:59 +0900

    rm ppython row files

---
+++ bin/setup.sh
@@ -0,0 +1,22 @@
+  981  python3 ../../test_cases/haihu/haihu_test.py
+  982  sudo pip install pyyaml
+  983  python3 ../../test_cases/haihu/haihu_test.py
+  984  wget https://pypi.python.org/pypi/distribute/0.7.3/distribute-0.7.3.zip
+  985  unzip ../../lib/python/distribute-0.7.3.zip -d ../../lib/python
+  986  python3 ../../lib/python/distribute-0.7.3/setup.py install
+  987  sudo python3 ../../lib/python/distribute-0.7.3/setup.py install
+  988  easy_install-2.7 pip3
+  989  sudo easy_install-2.7 pip3
+  990  sudo easy_install-2.7 pip
+  991  sudo pip install pyyaml
+  992  python3 ../../test_cases/haihu/haihu_test.py
+  993  ls /var/log
+  994  ls ../../etc/config.yml
+  995  python3 ../../test_cases/haihu/haihu_test.py
+  996  tail -F ../../var/log/log
+  997* python3 ../../test_cases/haihu/haihu_test.py
+  998* python3 ../../lib/haihu/get_tsumo_from.py
+  999* python3 ../../lib/haihu/tenhou.py
+ 1000* python3 ../../lib/haihu/get_tsumo_from.py
+ 1001* python3 ../../test_cases/haihu/haihu_test.py
+ 1002* cd ~/f/program/probabilities/tenhou/test_cases/workspace
